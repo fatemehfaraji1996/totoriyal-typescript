128a9d73dd61ce93a3fc3cb1311286ec26f00537
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-07-23 15:32:27 +0330

    commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 
 import { Route,Routes } from "react-router-dom"
 import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import{About} from "./pages/About"
+import {Store} from "./pages/Store"
+import{Navbar} from "./components/Navbar"
 function App() {
 
-  return <Container>
+  return(
+    <>
+    <Navbar/>
+   <Container className="mb-4">
     <Routes>
 
 <Route path="/" element={<Home/>}/>
@@ -13,6 +20,8 @@ function App() {
     </Routes>
 
   </Container>
+  </>
+  )
     
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,26 @@
+import{Button, Container, Nav,Navbar as NavbarBs}from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+
+export function Navbar() {
+    return(
+    <NavbarBs className='bg-white shadow-sm mb-3'>
+        <Container>
+        <Nav className='me-auto'>
+    <Nav.Link to="/" as={NavLink}>
+    Home
+    </Nav.Link>
+</Nav>
+<Nav>
+    <Nav.Link to="/store" as={NavLink}>
+    Store
+    </Nav.Link>
+</Nav>
+<Nav>
+    <Nav.Link to="/about" as={NavLink}>
+    About
+    </Nav.Link>
+</Nav>
+<Button style={{width:"3rem",height:"3rem"}}></Button>
+        </Container>
+    </NavbarBs>) 
+}
